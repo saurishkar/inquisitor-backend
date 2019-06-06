4a28a1a673036143a744de458fb192224f50f546
Saurish Kar <basicallysaurish@gmail.com>
2019-06-06 10:26:25 +0530

    Homepage (#1)
    * squashed
    
    * added edit and update action
    
    * Added update method for questions

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,4 +1,31 @@
 class QuestionsController < ApplicationController
   def index
+    @questions = Question.all
+  end
+
+  def create
+    @question = Question.new(permit_params)
+    @question.save
+    redirect_to @question
+  end
+
+  def show
+    @question = Question.find(params[:id])
+  end
+
+  def edit
+    @question = Question.find(params[:id])
+  end
+
+  def update
+    @question = Question.find(params[:id])
+    @question.update
+    redirect_to @question
+  end
+
+  private
+
+  def permit_params
+    params.require(:question).permit(:title, :description)
   end
 end
