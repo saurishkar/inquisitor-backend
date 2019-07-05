18cf97736c95dc4cba4466338af94e66bb2d9290
Saurish Kar <basicallysaurish@gmail.com>
2019-07-05 17:13:45 +0530

    minor change

--- config/deploy.rb
+++ config/deploy.rb
@@ -25,7 +25,7 @@ set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', '
 # set :pty, true
 
 # Default value for :linked_files is []
-append :linked_files, "config/database.yml", "config/secrets.yml", "config/aws.yml"
+append :linked_files, "config/database.yml", "config/secrets.yml", "config/aws.yml", "config/storage.yml"
 
 # Default value for linked_dirs is []
 # append :linked_dirs, "log", "tmp/pids", "tmp/cache", "tmp/sockets", "public/system"
